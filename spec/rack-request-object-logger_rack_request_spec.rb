60666cd3e4f223f7ccd64a5dcb3e75e7dc97bb7c
Ivan Stana <^_^@myrtana.sk>
2017-01-08 20:05:27 +0100

    refactor specs - deduplicate test data

--- spec/rack-request-object-logger_rack_request_spec.rb
+++ spec/rack-request-object-logger_rack_request_spec.rb
@@ -2,39 +2,19 @@
 require 'spec_helper'
 
 RSpec.describe 'RackRequestObjectLogger for rack-test request' do
-  let(:rack_test_headers) do
-    {
-      "rack.version" => [1, 3],
-      "rack.input" => StringIO.new('foo'),
-      "rack.errors" => StringIO.new('bar'),
-      "rack.multithread" => true,
-      "rack.multiprocess" => true,
-      "rack.run_once" => false,
-      "rack.test" => true,
-      "rack.url_scheme" => "http",
-      "REQUEST_METHOD" => "GET",
-      "SERVER_NAME" => "example.org",
-      "SERVER_PORT" => "80",
-      "QUERY_STRING" => "",
-      "PATH_INFO" => "/",
-      "HTTPS" => "off",
-      "SCRIPT_NAME" => "",
-      "CONTENT_LENGTH" => "0",
-      "REMOTE_ADDR" => "127.0.0.1",
-      "HTTP_HOST" => "example.org",
-      "HTTP_COOKIE" => ""
-    }
-  end
+  include_context 'spec_data'
 
   let(:app) { proc{ [200, {}, ['Hello, world.']] } }
   let(:stack) { RackRequestObjectLogger.new(app, RequestDummyLog) }
   let(:request) { Rack::MockRequest.new(stack) }
 
   let(:logger_object) { RequestDummyLog.new }
-  before { allow(RequestDummyLog).to receive(:new).and_return(logger_object) }
+  before do
+    allow(RequestDummyLog).to receive(:new).and_return(logger_object)
+    set_headers(rack_test_headers)
+  end
 
   it 'logs HTTP headers and CGI headers' do
-    stub_const("Rack::MockRequest::DEFAULT_ENV", rack_test_headers)
     response = request.get('/')
     expect(logger_object.data['REQUEST_METHOD']).to eq('GET')
     expect(logger_object.data['SERVER_NAME']).to eq('example.org')
@@ -49,13 +29,11 @@ RSpec.describe 'RackRequestObjectLogger for rack-test request' do
   end
 
   it 'does not log cookies' do
-    stub_const("Rack::MockRequest::DEFAULT_ENV", rack_test_headers)
     response = request.get('/')
     expect(logger_object.data['HTTP_COOKIE']).to eq(nil)
   end
 
   it 'does not log rack.* values' do
-    stub_const("Rack::MockRequest::DEFAULT_ENV", rack_test_headers)
     response = request.get('/')
     expect(logger_object.data['rack.version']).to eq(nil)
     expect(logger_object.data['rack.input']).to eq(nil)
@@ -69,7 +47,6 @@ RSpec.describe 'RackRequestObjectLogger for rack-test request' do
 
   it 'generates random uid when request ID is not present' do
     allow(SecureRandom).to receive(:uuid).and_return('6cd406c9-2d24-4117-b8bc-924950243979')
-    stub_const("Rack::MockRequest::DEFAULT_ENV", rack_test_headers)
     response = request.get('/')
     expect(logger_object.uid).to eq('6cd406c9-2d24-4117-b8bc-924950243979')
   end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,13 +1,6 @@
 # frozen_string_literal: true
 require 'bundler/setup'
 $LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
-require "rack-request-object-logger"
+require 'rack-request-object-logger'
 require 'pry'
-
-class RequestDummyLog
-  attr_accessor :application_server_request_start, :application_server_request_end, :data, :uid
-
-  def save
-    true
-  end
-end
+require 'spec_data'
